fixed typos, fixed error in add_mask_tokens_and_unshuffle with incorrect mask token calculation

mae_arch.py
@@ -111,6 +111,8 @@ class MAE(nn.Module):
         x = self.encoder_norm(x)
         #encoder and decoder have different depths - linear transformation to handle this as in MAE paper 
         x= self.enc_to_dec(x) # this is our patch embedding for the decoder, no need to linearly project again. in self.embed_patch_decoder
+
+        # print(unshuffle_indices)
         x = self.add_mask_tokens_and_unshuffle(x,unshuffle_indices)
         
         # linear project just need pos
@@ -217,6 +219,8 @@ class MAE(nn.Module):
         # x should be of shape batch_size, seq_length, D]
         
         batch_size = x.shape[0]
+
+        # print(x.shape)
         
 
 
@@ -257,10 +261,14 @@ class MAE(nn.Module):
         # add mask tokens
         #we removed 
         #n_masktokens per batch 
+
         
-        mask_tokens = self.mask_token.repeat(x.shape[0],self.n_mask_tokens,1)
+        
+        mask_tokens = self.mask_token.repeat(x.shape[0],self.n_mask_tokens+1,1)
         # add mask tokens to end 
         x_with_masks = torch.cat((x, mask_tokens), dim=1)
+
+        # print(x_with_masks.shape)
         #unshuffle tokens (check size )
         x_with_masks_unshuffled = torch.stack([x_with_masks[i, idx] for i, idx in enumerate(inverse_indices)])
         return x_with_masks_unshuffled

run_mae.py
@@ -93,6 +93,7 @@ def args_parser():
 
 
     args = parser.parse_args()
+    print(args)
     return args
 
 
@@ -107,6 +108,7 @@ if __name__ == "__main__":
     
     
     args.device = torch.device("cuda" if args.cuda and torch.cuda.is_available() else "cpu")
+    print(args.device)
 
 
     # directory for model checkpoints -
@@ -132,5 +134,5 @@ if __name__ == "__main__":
     print(f'Initialized_trainer')
     
     # useful to examine for each run, needed for plot_train
-    save_config(args)
-    trainer.train()
+    # save_config(args)
+    trainer.train_model()

train_mae.py
@@ -37,7 +37,7 @@ class Trainer:
         self.device = self.args.device 
         self.train_loader, self.val_loader, self.mean_pixels, self.std_pixels  = get_loaders(self.args)
         # also gets means and stds for unnormalizing
-        print('Created dataset loaders using dataset in {self.args.dataset} ')
+        print(f'Created dataset loaders using dataset in {self.args.dataset} ')
 
 
         self.mae = MAE(self.args) 
@@ -47,7 +47,7 @@ class Trainer:
 
         self.base_lr = 1.5e-4
         self.lr = self.base_lr * (self.args.batch_size/256)
-        self.optimizer =torch.optim.AdamW(MAE.parameters(), lr=self.lr)
+        self.optimizer =torch.optim.AdamW(self.mae.parameters(), lr=self.lr)
 
 
         