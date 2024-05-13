4e6b21178d3b75fce7fc5727a06aca719c5bd6d8
Jeffrey Benchu Li <66740889+JeliPenguin@users.noreply.github.com>
2024-05-14 00:41:43 +0100

    uncommented install requiresments

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version='0.1',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    # install_requires=[
-    #     'torch==2.2',
-    #     'torchvision==0.17',
-    #     'torchmetrics',
-    #     'datasets',
-    #     'matplotlib'
-    # ],
+    install_requires=[
+        'torch==2.2',
+        'torchvision==0.17',
+        'torchmetrics',
+        'datasets',
+        'matplotlib'
+    ],
     python_requires='>=3.10',
     packages=find_packages(),
 )
